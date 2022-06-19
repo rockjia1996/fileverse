7f043b2166428814958953bef3191dbfd2362cbf
Yu Jia <rockjia1996@gmail.com>
2022-06-19 13:14:33 -0400

    Add routes, /, /login, /files.

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 
 
-
-@app.route('/')
+@app.route("/")
+@app.route("/index.html")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/files")
+def files():
+    return render_template("files.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
