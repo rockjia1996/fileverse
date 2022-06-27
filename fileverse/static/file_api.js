f2e3b6e0e8ac53dc0f9ed8f16a6cb039ac633f4d
Yu Jia <rockjia1996@gmail.com>
2022-06-26 20:29:14 -0400

    Implement download frontend.

--- fileverse/static/file_api.js
+++ fileverse/static/file_api.js
@@ -22,12 +22,21 @@ async function deleteFile(event){
 async function downloadFile(event){
     const  id = event.target.parentElement.parentElement.id;
     const  download_request = "/files/download/" + id;
+
+    const download_link = document.createElement('a')
+    
+    download_link.download = event.target.parentElement.parentElement.childNodes[1]
+    download_link.href = download_request
+    download_link.click()
+
+    /*
     response = await fetch(download_request, {method: "GET"});
     console.log(response)
 
     if (response.status === 200){
 
     }
+    */
 
 }
 
