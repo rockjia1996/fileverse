b8585f474510a927676c1333a12cd4c51c0c8b06
Yu Jia <rockjia1996@gmail.com>
2022-07-06 14:37:39 -0400

    Refactor the table.js with refactorTable.js

--- fileverse/static/js/files/refactorTable.js
+++ fileverse/static/js/files/refactorTable.js
@@ -1,12 +1,12 @@
 
-function initTable(){
+function initTable() {
     const contextMenu = document.getElementById("context-menu");
     const scopes = document.querySelectorAll(".file-table__row");
 
     // Prevent the default behavior, if user right click out side the file enties.
     document.querySelector(".file-table")
-            .firstElementChild
-            .addEventListener("contextmenu", event => event.preventDefault());
+        .firstElementChild
+        .addEventListener("contextmenu", event => event.preventDefault());
 
     highlightLeftClick(scopes);
 
@@ -14,21 +14,80 @@ function initTable(){
 
 
 //  Highlight the file entry while left click
-function highlightLeftClick(entries){
+function highlightLeftClick(entries) {
     entries.addEventListener("click", event => {
         // Note that event.target is the a <td> cell, so its parent is row
         event.preventDefault();
         const rows = event.target.parentElement.parentElement.children;
 
-        for (row of rows)  
+        for (row of rows)
             row.classList.remove("active-row");
-        
+
         event.target.parentElement.classList.add("active-row")
     });
 }
 
 
 // Customize right click context menu
-function customizeContextMenu(entries){
-    
+function customizeContextMenu(entries) {
+    entries.addEventListener("contextmenu", event => {
+        event.preventDefault();
+
+        const { clientX: mouseX, clientX: mouseY } = event;
+        contextMenu.style.top = `${mouseY}px`;
+        contextMenu.style.left = `${mouseX}px`;
+        contextMenu.classList.add("visible");
+
+        /* 
+            Note that event.target is  <td> that is clicked on, 
+            event.target.parentElement is <tr>
+        */
+        const rows = event.target.parentElement.parentElement.children;
+
+        for (row of rows)
+            row.classList.remove("active-row");
+
+        event.target.parentElement.classList.add("active-row");
+
+        const entry = event.target.parentElement;
+        const fileId = entry.id;
+        const options = document.getElementById("context-menu").children;
+
+        options[0].onclick = () => {
+            downloadFile(fileId);
+            document.getElementById("context-menu").classList.remove("visible")
+        }
+
+        options[1].onclick = () => {
+            deleteFile(fileId, entry)
+            document.getElementById("context-menu").classList.remove("visible")
+        }
+    });
+}
+
+// Close the context menu if user left click outside the menu
+function updateHighlight(entries){
+    entries.forEach(entry => {
+        entry.addEventListener("click", e => {
+            if (e.target.offsetParent != contextMenu) {
+                contextMenu.classList.remove("visible");
+            }
+        });
+    });
+}
+
+// Right click in another area 
+function openContextMenuInNewLocation(entries){
+    entries.forEach(entry => {
+        entry.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            const { clientX: mouseX, clientY: mouseY } = event;
+            contextMenu.style.top = `${mouseY}px`;
+            contextMenu.style.left = `${mouseX}px`;
+            contextMenu.classList.remove("visible");
+            setTimeout(() => {
+                contextMenu.classList.add("visible");
+            });
+        })
+    })
 }
