bec007e58698b01f2aee30abdf2ba0327fe68a28
Yu Jia <rockjia1996@gmail.com>
2022-07-07 14:18:42 -0400

    Delete the unwanted file refactorTable.js.

--- fileverse/static/js/files/table.js
+++ fileverse/static/js/files/table.js
@@ -1,119 +1,121 @@
-const contextMenu = document.getElementById("context-menu");
-//const scope = document.querySelector("body");
-//const scope = document.querySelector(".file-table__body")
-const scopes = document.querySelectorAll(".file-table__row")
+initTable();
 
-document.querySelector(".file-table")
-    .firstElementChild
-    .addEventListener("contextmenu", event => {
-        event.preventDefault();
-    })
+function initTable() {
+    const contextMenu = document.getElementById("context-menu");
+    const scopes = document.querySelectorAll(".file-table__row");
 
-// Left click hightlight file entry
-scopes.forEach(scope => {
-    scope.addEventListener("click", event => {
-        event.preventDefault();
-        const rows = event.target.parentElement.parentElement.children;
+    // Prevent the default behavior, if user right click out side the file enties.
+    document.querySelector(".file-table")
+        .firstElementChild
+        .addEventListener("contextmenu", event => event.preventDefault());
 
-        for (row of rows)
-            row.classList.remove("active-row")
-
-        event.target.parentElement.classList.add("active-row")
-    })
-});
-
-// Right click pops context menu
-scopes.forEach(scope => {
-    scope.addEventListener("contextmenu", event => {
-        event.preventDefault();
-        const { clientX: mouseX, clientY: mouseY } = event;
-        contextMenu.style.top = `${mouseY}px`;
-        contextMenu.style.left = `${mouseX}px`;
-        contextMenu.classList.add("visible");
-
-        const rows = event.target.parentElement.parentElement.children;
-
-        for (row of rows)
-            row.classList.remove("active-row")
-        event.target.parentElement.classList.add("active-row");
-
-        const entry = event.target.parentElement;
-        const fileId = entry.id;
-        const options = document.getElementById("context-menu").children;
-
-        options[0].onclick = () => {
-            downloadFile(fileId);
-            document.getElementById("context-menu").classList.remove("visible")
-        }
-
-        options[1].onclick = () => {
-            deleteFile(fileId, entry) 
-            document.getElementById("context-menu").classList.remove("visible")
-        } 
-    })
-});
-
-scopes.forEach(scope => {
-    scope.addEventListener("click", e => {
-        if (e.target.offsetParent != contextMenu) {
-            contextMenu.classList.remove("visible");
-        }
-    })
-})
-
-scopes.forEach(scope => {
-    scope.addEventListener("contextmenu", (event) => {
-        event.preventDefault();
-        const { clientX: mouseX, clientY: mouseY } = event;
-        contextMenu.style.top = `${mouseY}px`;
-        contextMenu.style.left = `${mouseX}px`;
-        contextMenu.classList.remove("visible");
-        setTimeout(() => {
-            contextMenu.classList.add("visible");
-        });
-    })
-})
-
-
-/*
-const normalizePosition = (mouseX, mouseY) => {
-    const {
-        left: scopeOffsetX,
-        top: scopeOffsetY,
-    } = scope.getBoundingClientRect();
-
-    const scopeX = mouseX - scopeOffsetX;
-    const scopeY = mouseY - scopeOffsetY;
-
-    const outOfBoundsOnX = scopeX + contextMenu.clientWidth > scope.clientWidth;    
-    const outOfBoundsOnY = scopeY + contextMenu.clientHeight > scope.clientHeight;
-
-    let normalizedX = mouseX;
-    let normalizedY = mouseY;
-
-    if (outOfBoundsOnX){
-        normalizedX = scopeOffsetX + scope.clientWidth - contextMenu.clientWidth;
-    }
-
-    if (outOfBoundsOnY){
-        normalizedY = scopeOffsetY + scope.clientHeight - contextMenu.clientHeight;
-    }
-    return { normalizedX, normalizedY};
+    highlightLeftClick(scopes);
+    customizeContextMenu(scopes, contextMenu);
+    updateHighlight(scopes, contextMenu);
+    openContextMenuInNewLocation(scopes, contextMenu);
 }
 
-scope.addEventListener("contextmenu", event =>{
-    event.preventDefault();
+//  Highlight the file entry while left click
+function highlightLeftClick(entries) {
+    entries.forEach(entry => {
+        entry.addEventListener("click", event => {
+            // Note that event.target is the a <td> cell, so its parent is row
+            event.preventDefault();
+            const rows = event.target.parentElement.parentElement.children;
 
-    const { offsetX: mouseX, offsetY: mouseY } = event;
-    const { normalizedX, normalizedY } = normalizePosition(mouseX, mouseY);
+            for (row of rows)
+                row.classList.remove("active-row");
 
-    contextMenu.style.top = `${normalizedY}px`;
-    contextMenu.style.left = `${normalizedX}px`;
+            event.target.parentElement.classList.add("active-row")
+        });
 
-    contextMenu.classList.remove("visible");
 
-    setTimeout(() => {
-        contextMenu.classList.add("value");
     })
-})
-*/
+}
+
+// Customize right click context menu
+function customizeContextMenu(entries, contextMenu) {
+    entries.forEach(entry => {
+        entry.addEventListener("contextmenu", event => {
+
+            //const contextMenu = document.getElementById("context-menu");
+            event.preventDefault();
+
+            const { clientX: mouseX, clientY: mouseY } = event;
+            contextMenu.style.top = `${mouseY}px`;
+            contextMenu.style.left = `${mouseX}px`;
+            contextMenu.classList.add("visible");
+            
+            /* 
+                Note that event.target is  <td> that is clicked on, 
+                event.target.parentElement is <tr>
+            */
+            const rows = event.target.parentElement.parentElement.children;
+    
+            for (row of rows)
+                row.classList.remove("active-row");
+    
+            event.target.parentElement.classList.add("active-row");
+    
+            const entry = event.target.parentElement;
+            const fileId = entry.id;
+            const options = document.getElementById("context-menu").children;
+    
+            options[0].onclick = () => {
+                downloadFile(fileId);
+                document.getElementById("context-menu").classList.remove("visible")
+            }
+    
+            options[1].onclick = () => {
+                deleteFile(fileId, entry)
+                document.getElementById("context-menu").classList.remove("visible")
+            }
+        });
+    });
+}
+
+// Close the context menu if user left click outside the menu
+function updateHighlight(entries, contextMenu) {
+    entries.forEach(entry => {
+        entry.addEventListener("click", e => {
+            if (e.target.offsetParent != contextMenu) {
+                contextMenu.classList.remove("visible");
+            }
+        });
+    });
+}
+
+// Right click in another area 
+function openContextMenuInNewLocation(entries, contextMenu) {
+    entries.forEach(entry => {
+        entry.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            const { clientX: mouseX, clientY: mouseY } = event;
+            contextMenu.style.top = `${mouseY}px`;
+            contextMenu.style.left = `${mouseX}px`;
+            contextMenu.classList.remove("visible");
+            setTimeout(() => {
+                contextMenu.classList.add("visible");
+            });
+        })
+    })
+}
+
+// Update the file table UI
+function updateFileEntry(id, filename, date, size){
+    const table = document.querySelector(".file-table__body");
+
+    const newEntry = table.insertRow(0);
+    newEntry.id = id;
+    
+    const filenameCell = newEntry.insertCell(0)
+    const dateCell = newEntry.insertCell(1);
+    const sizeCell = newEntry.insertCell(2);
+
+    filenameCell.appendChild(document.createTextNode(filename));
+    dateCell.appendChild(document.createTextNode(date));
+    sizeCell.appendChild(document.createTextNode(size));
+
+    return newEntry;
+
+}
