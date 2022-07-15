db6a0d64ebba2afce545780203c42227c9ed7135
Yu Jia <rockjia1996@gmail.com>
2022-07-15 14:00:15 -0400

    Implement adding file entry.

--- fileverse/static/js/mini-files/mini-table.js
+++ fileverse/static/js/mini-files/mini-table.js
@@ -1,3 +1,9 @@
-function addFileEntryUI(){
+function addTableRow(table, cells){
+    let newRow = table.insertRow(0);
+    let numOfCells = cells.length;
 
+    for (let i = 0; i < numOfCells; i++){
+        currentCell = newRow.insertCell(i);
+        currentCell.appendChild(document.createTextNode(cells[i]));
+    }
 }
