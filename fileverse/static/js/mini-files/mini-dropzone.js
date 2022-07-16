c1b02693228eb16b36fa06ce0466438b7d15992a
Yu Jia <rockjia1996@gmail.com>
2022-07-16 09:51:47 -0400

    Implemented create upload entry.

--- fileverse/static/js/mini-files/mini-dropzone.js
+++ fileverse/static/js/mini-files/mini-dropzone.js
@@ -1,4 +1,59 @@
+initDropzone()
+
+function initDropzone(){
+
+    const inputElement = document.querySelector(".drop-zone__input");
+    const dropZoneElement = document.querySelector(".drop-zone");
+    
+    inputElement.addEventListener("change", e => {
+        for (selected of inputElement.files) {
+            // TO DO: implement the process of each file
+            const table = document.getElementById("upload-table").children[1]
+            addTableRow(table, createUploadEntryArray(selected.name))
+
+        }
+    } )
+    
+    dropZoneElement.addEventListener("dragover", e => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+    
+    dropZoneElement.addEventListener("click", e => {
+        inputElement.click();
+    })
+    
+    dropZoneElement.addEventListener("dragleave", e => {
+            dropZoneElement.classList.remove("drop-zone--over")
+    } )
+    
+    dropZoneElement.addEventListener("dragend", e => {
+        dropZoneElement.classList.remove("drop-zone--over")
+    } )
+    
+    dropZoneElement.addEventListener("drop", e => {
+        e.preventDefault();
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+    
+    
+            for (selected of inputElement.files) {
+                // TO DO: implement the process of each file
+                console.log(selected.name)
+            }
+    
+        }
+        dropZoneElement.classList.remove("drop-zone--over");
+    })
+
+
+
+}
+
+
+
 function getSelectedFiles(){
+    const dropzoneFiles = document.querySelector(".drop-zone");
 
 }
 
--- fileverse/static/js/mini-files/mini-table.js
+++ fileverse/static/js/mini-files/mini-table.js
@@ -4,6 +4,26 @@ function addTableRow(table, cells){
 
     for (let i = 0; i < numOfCells; i++){
         currentCell = newRow.insertCell(i);
-        currentCell.appendChild(document.createTextNode(cells[i]));
+        currentCell.appendChild(cells[i]);
     }
+}
+
+function createUploadEntryArray(name){
+    const filename = document.createTextNode(name);
+    const progressBar = document.createElement("progress");
+    const cancelButton = document.createElement("button");
+
+    progressBar.max = "100";
+    progressBar.value = "0";
+    cancelButton.appendChild(document.createTextNode("Cancel"))
+
+    return [filename, progressBar, cancelButton]
+}
+
+
+function createFileEntryArray(id, name, date, size){
+    const filename = document.createTextNode(name);
+    const modified = document.createTextNode(date);
+    const fileSize = document.createTextNode(size);
+
 }
