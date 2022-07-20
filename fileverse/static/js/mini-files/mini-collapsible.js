5fa23e10dfc2ccea00a254f97a44dcc642e54124
Yu Jia <rockjia1996@gmail.com>
2022-07-20 12:21:34 -0400

    Add collapsible file table.

--- fileverse/static/js/mini-files/mini-collapsible.js
+++ fileverse/static/js/mini-files/mini-collapsible.js
@@ -1,9 +1,29 @@
 
 initCollapsibleUploadTable();
+initCollapsibleFileTable();
 
 function initCollapsibleUploadTable(){
-    const collapsible = document.querySelector(".collapsible");
-    const content = collapsible.nextElementSibling;
+    const content = document.querySelector("#upload-table");
+    const collapsible = content.previousElementSibling;
+
+    content.style.display = "none";
+
+    collapsible.addEventListener("click", () => {
+        collapsible.classList.toggle("active");
+        const content = collapsible.nextElementSibling;
+
+
+        if (content.style.display === "none")
+            content.style.display = "";
+        else
+            content.style.display = "none";
+    });
+}
+
+function initCollapsibleFileTable(){
+    const content = document.querySelector("#file-table");
+    const collapsible = content.previousElementSibling;
+
 
     content.style.display = "none";
 
