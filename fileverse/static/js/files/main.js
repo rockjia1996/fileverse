94f67b56c4a65a1c1f425811848f3d3c8219bcd7
Yu Jia <rockjia1996@gmail.com>
2022-08-12 04:56:41 -0400

    Add equals() method in FileNode class.

--- fileverse/static/js/files/main.js
+++ fileverse/static/js/files/main.js
@@ -13,11 +13,8 @@ const data = [
 ]
 
 
-function FileNode(id, name, size, date, type, parent, children) {
-    this.id = id;
-    this.name = name;
-    this.size = size;
-    this.date = date;
+function FileNode(content, type, parent, children) {
+    this.content = content;
     this.type = type;
 
     this.parent = parent;
@@ -25,10 +22,17 @@ function FileNode(id, name, size, date, type, parent, children) {
 
     this.addChild = (child) => this.children.push(child);
     this.addParent = (parent) => this.parent = parent;
+
+    this.equals = (target) => {
+        if (this.content.id === target.content.id)
+            return true;
+        else
+            return false;
+    }
 }
 
-function FileTree(root){
-    this.root =  root;
+function FileTree(root) {
+    this.root = root;
     this.currentPath = [];
 
     this.visited = [];
@@ -48,15 +52,15 @@ function FileTree(root){
     }
 
     this.dfs = (current, target) => {
-        if (current.id === target.id) 
+        if (current.equals(target))
             return current;
 
         this.visited.push(current);
         const children = current.children;
 
-        for (child of children){
+        for (child of children) {
             let result = null;
-            if (!this.visited.some(v => v.id === child.id))
+            if (!this.visited.some(v => v.equals(child)))
                 result = this.dfs(child, target);
             if (result)
                 return result;
@@ -64,15 +68,27 @@ function FileTree(root){
     }
 }
 
-const root = new FileNode(0, "root node", null , null, "folder", null, []);
+const root = new FileNode({ id: 0 }, "folder", null, []);
 
-const node1 = new FileNode(1, null, null , null, "folder", root, []);
-const node2 = new FileNode(2, null, null , null, "folder", root, []);
-const node3 = new FileNode(3, null, null , null, "folder", root, []);
+const node1 = new FileNode({ id: 1 }, "folder", root, []);
+const node2 = new FileNode({ id: 2 }, "folder", root, []);
+const node3 = new FileNode({ id: 3 }, "folder", root, []);
 
-const node4 = new FileNode(4, null, null , null, "folder", node3, []);
-const node5 = new FileNode(5, null, null , null, "folder", node3, []);
-const node6 = new FileNode(6, null, null , null, "folder", node3, []);
+const node4 = new FileNode({ id: 4 }, "folder", node3, []);
+const node5 = new FileNode({ id: 5 }, "folder", node3, []);
+const node6 = new FileNode({ id: 6 }, "folder", node3, []);
+
+
+const node7 = new FileNode({ id: 7 }, "folder", node5, []);
+const node8 = new FileNode({ id: 8 }, "folder", node5, []);
+const node9 = new FileNode({ id: 9 }, "folder", node5, []);
+const node10 = new FileNode({ id: 10 }, "folder", node5, []);
+
+const node11 = new FileNode({ id: 11 }, "folder", node1, []);
+const node12 = new FileNode({ id: 12 }, "folder", node11, []);
+const node13 = new FileNode({ id: 13 }, "folder", node11, []);
+const node14 = new FileNode({ id: 14 }, "folder", node11, []);
+
 
 root.addChild(node1);
 root.addChild(node2);
@@ -82,5 +98,16 @@ node3.addChild(node4);
 node3.addChild(node5);
 node3.addChild(node6);
 
+node5.addChild(node7);
+node5.addChild(node8);
+node5.addChild(node9);
+node5.addChild(node10);
+
+node1.addChild(node11);
+node11.addChild(node12);
+node11.addChild(node13);
+node11.addChild(node14);
+
+
 const testTree = new FileTree(root);
-console.log(testTree.searchNode(node6));
+console.log(testTree.searchNode(node8));
