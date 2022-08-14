9cfd31d4dfd41c596d3f718d118264b97e5df5ab
Yu Jia <rockjia1996@gmail.com>
2022-08-14 10:55:34 -0400

    Remove the old implementation that considers a nest folder.

--- fileverse/static/js/files/main.js
+++ fileverse/static/js/files/main.js
@@ -20,62 +20,9 @@ function fakeDataAPI(num){
 let data = fakeDataAPI(10);
 console.log(data)
 
-/*
-const root = new FileNode({ id: 0 }, "folder", null, []);
 
-const node1 = new FileNode({ id: 1 }, "folder", root, []);
-const node2 = new FileNode({ id: 2 }, "folder", root, []);
-const node3 = new FileNode({ id: 3 }, "folder", root, []);
-
-const node4 = new FileNode({ id: 4 }, "folder", node3, []);
-const node5 = new FileNode({ id: 5 }, "folder", node3, []);
-const node6 = new FileNode({ id: 6 }, "folder", node3, []);
-
-
-const node7 = new FileNode({ id: 7 }, "folder", node5, []);
-const node8 = new FileNode({ id: 8 }, "folder", node5, []);
-const node9 = new FileNode({ id: 9 }, "folder", node5, []);
-const node10 = new FileNode({ id: 10 }, "folder", node5, []);
-
-const node11 = new FileNode({ id: 11 }, "folder", node1, []);
-const node12 = new FileNode({ id: 12 }, "folder", node11, []);
-const node13 = new FileNode({ id: 13 }, "folder", node11, []);
-const node14 = new FileNode({ id: 14 }, "folder", node11, []);
-
-
-root.addChild(node1);
-root.addChild(node2);
-root.addChild(node3);
-
-node3.addChild(node4);
-node3.addChild(node5);
-node3.addChild(node6);
-
-node5.addChild(node7);
-node5.addChild(node8);
-node5.addChild(node9);
-node5.addChild(node10);
-
-node1.addChild(node11);
-node11.addChild(node12);
-node11.addChild(node13);
-node11.addChild(node14);
-
-
-const testTree = new FileTree(root);
-console.log(testTree.searchNode(node8));
-*/
-
-
-function FileNode(content, type, parent, children) {
+function FileNode(content) {
     this.content = content;
-    this.type = type;
-
-    this.parent = parent;
-    this.children = children;
-
-    this.addChild = (child) => this.children.push(child);
-    this.addParent = (parent) => this.parent = parent;
 
     this.equals = (target) => {
         if (this.content.id === target.content.id)
@@ -85,50 +32,39 @@ function FileNode(content, type, parent, children) {
     }
 
     this.onDownload = () => {
+        const { id } = this.content;    
+        const downloadURL = "/files/download/" + id;
+        const downloadLink = document.createElement('a');
+        downloadLink.href = downloadURL;
+        downloadLink.click();
     }
 
-    this.onDelete =() => {
+    this.onDelete = async () => {
+        const { id } = this.content;    
+        const deleteRequest = "/files/delete/" + id;
+        const response = await fetch(deleteRequest, { method: "DELETE" });
+        return response.status;
     }
 
 
 }
 
-function FileTree(root) {
-    this.root = root;
-    this.currentPath = [];
-
-    this.visited = [];
-
-    this.addNode = (parent, node) => {
-        const parentNode = this.searchNode(parent);
-        parentNode.addChild(node);
+function FileTree() {
+    this.nodes = [];
+    
+    this.addNode = (target) => {
+        this.nodes.push(target);
     }
 
-    this.removeNode = (node) => {
-    }
-
-    this.searchNode = (target) => {
-        const found = this.dfs(this.root, target);
-        this.visited = [];
-        return found;
-    }
-
-    this.dfs = (current, target) => {
-        if (current.equals(target))
-            return current;
-
-        this.visited.push(current);
-        const children = current.children;
-
-        for (child of children) {
-            let result = null;
-            if (!this.visited.some(v => v.equals(child)))
-                result = this.dfs(child, target);
-            if (result)
-                return result;
-        }
+    this.deleteNode = (target) => {
+        const index = this.node.findIndex(node => node.equals(target));
+        this.node[index].onDelete();
+        this.nodes.splice(index, 1);
     }
 }
+
+
+
 
 function Controller() {
 
